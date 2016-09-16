968bc710415616ff3c75d08d856326088f4b17f3
Nic McDonald <n.mcdonald83@gmail.com>
2016-09-16 11:40:45 -0600

    added html header and footer

--- gfmapi.py
+++ gfmapi.py
@@ -21,7 +21,30 @@ try:
 
   request = urllib.request.Request(url, data=bytes, headers=headers)
   result = urllib.request.urlopen(request).read().decode('utf-8')
+
+  html_header = (
+    '<?xml version="1.0" encoding="UTF-8" ?> '
+    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" '
+    '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
+    '<html xmlns="http://www.w3.org/1999/xhtml"> '
+    '<head> '
+    '<title>Markdown Rendering from GitHub</title> '
+    '<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>  '
+    '<link rel="stylesheet" type="text/css" '
+    'href="https://cdn.rawgit.com/sindresorhus/github-markdown-css/'
+    'gh-pages/github-markdown.css"> '
+    '<style>.markdown-body {box-sizing: border-box; min-width: 200px; '
+    'max-width: 980px; margin: 0 auto; padding: 45px; }</style>'
+    '</head>'
+    '<body class="markdown-body">')
+
+  html_footer = (
+    '</body>'
+    '</html>')
+
+  print(html_header)
   print(result)
+  print(html_footer)
 
 except http.client.BadStatusLine:
   traceback.print_exc()
